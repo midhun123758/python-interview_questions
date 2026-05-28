96f784fbc99ac90019ca53a9abdc5587422ee037
midhun123758 <midhunchandran230@gmail.com>
2026-05-28 14:22:36 +0530

    5/28

--- practicals.py
+++ practicals.py
@@ -36,3 +36,15 @@ def duplicate(char):
 
 duplicate("midhunn")
          
+###nums = [2,7,11,15]target = 9 Output:[0,1]##
+
+def find_target(nums,target):
+    for i in range(len(nums)):
+        for j in  range(i+1,len(nums)):
+            if nums[i]+nums[j]==target:
+                return print([i,j])
+
+find_target([2,7,11,15],10)
+
+
+
